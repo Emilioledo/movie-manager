5a25cc73d75557baaac6627c14dc566370b04b5e
emilioledo <fernandezledoemilio@gmail.com>
2025-03-08 00:31:38 -0300

    feat: enhance authentication module with bcrypt password hashing and update user registration/login methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,27 @@
 import {
   Body,
   Controller,
-  Get,
   HttpCode,
   HttpStatus,
   Post,
   ValidationPipe,
-} from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthDto } from './dto/auth.dto';
+} from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto/auth.dto";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @HttpCode(HttpStatus.OK)
-  @Post('register')
-  async signUp(@Body(ValidationPipe) authDto: AuthDto) {
-    return this.authService.signUp(authDto);
+  @HttpCode(HttpStatus.CREATED)
+  @Post("register")
+  async register(@Body(ValidationPipe) authDto: AuthDto) {
+    return this.authService.register(authDto);
   }
 
   @HttpCode(HttpStatus.OK)
-  @Post('login')
-  async signIn(@Body(ValidationPipe) authDto: AuthDto) {
-    return this.authService.signIn(authDto);
+  @Post("login")
+  async login(@Body(ValidationPipe) authDto: AuthDto) {
+    return this.authService.login(authDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthDto } from "./dto/auth.dto";
 import { JwtService } from "@nestjs/jwt";
 import { User } from "./interfaces/auth.interface";
 
+import * as bcrypt from "bcrypt";
+
 @Injectable()
 export class AuthService {
   constructor(private jwtService: JwtService) {}
+
+  saltOrRounds: number = 10;
 
   users: User[] = [
     { id: 1, username: "john", password: "changeme", rol: "admin" },
@@ -15,16 +19,21 @@ export class AuthService {
     return this.users.find((user) => user.username === username)!;
   }
 
-  signUp(authDto: AuthDto) {
+  async register(authDto: AuthDto) {
     const { username, password } = authDto;
-    const newUser: User = { id: 2, username, password, rol: "admin" };
+    const hashPass = await bcrypt.hash(password, this.saltOrRounds);
+    const newUser: User = { id: 2, username, password: hashPass, rol: "user" };
     this.users.push(newUser);
     return true;
   }
 
-  async signIn(authDto: AuthDto): Promise<{ access_token: string }> {
-    const { username } = authDto;
+  async login(authDto: AuthDto): Promise<{ access_token: string }> {
+    const { username, password } = authDto;
     const user = this.findOne(username);
+    const isPasswordMatch = await bcrypt.compare(password, user?.password);
+    if (!isPasswordMatch) {
+      throw new UnauthorizedException();
+    }
     const payload = { sub: user.id, username: user.username };
     return {
       access_token: await this.jwtService.signAsync(payload),
